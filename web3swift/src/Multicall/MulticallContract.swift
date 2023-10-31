44996c24567105529b73880185b1ccca7fd8e587
13071806115 <>
2023-10-31 18:04:41 +0800

    add env

--- web3swift/src/Multicall/MulticallContract.swift
+++ web3swift/src/Multicall/MulticallContract.swift
@@ -6,14 +6,35 @@
 import BigInt
 import Foundation
 
+public let multicallContainer = AppEnv.shared
+
+public protocol MulticallAppEnvDelegate: AnyObject {
+    func getEnvironmentIsDebug() -> Bool
+}
+
+open class AppEnv: NSObject {
+    static var shared = AppEnv()
+    public weak var multiCallDelegate: MulticallAppEnvDelegate?
+}
+
 extension Multicall {
     public enum Contract {
+        case common
         static let goerliAddress: EthereumAddress = "0x77dCa2C955b15e9dE4dbBCf1246B4B85b651e50e"
         static let mainnetAddress: EthereumAddress = "0xF34D2Cb31175a51B23fb6e08cA06d7208FaD379F"
-        static let multicall2Address: EthereumAddress = "0xfABB392F116420b817B639D59bDbfCBbC505487E"
+        var multicall2Address: EthereumAddress  {
+            return  self.getEnvironmentDebug() ? EthereumAddress("0xaF20cB65A32e3ac0890A81191b9b211965f6A5cC") : EthereumAddress("0xfABB392F116420b817B639D59bDbfCBbC505487E")
+        }
+        
+        public func getEnvironmentDebug() -> Bool {
+            guard let debug = multicallContainer.multiCallDelegate?.getEnvironmentIsDebug() else {
+                return true
+            }
+            return debug
+        }
 
-        public static func registryAddress(for network: EthereumNetwork) -> EthereumAddress? {
-            return Self.multicall2Address
+        public func registryAddress(for network: EthereumNetwork) -> EthereumAddress? {
+            return  self.multicall2Address
 //            switch network {
 //            case .mainnet:
 //                return Self.mainnetAddress
@@ -25,7 +46,8 @@ extension Multicall {
         }
 
         public enum Functions {
-            public struct aggregate: ABIFunction {
+            public struct aggregate: ABIMultiFunction {
+                public var funcName: String = "aggregate"
                 public static let name = "aggregate"
                 public let gasPrice: BigUInt?
                 public let gasLimit: BigUInt?
@@ -52,7 +74,8 @@ extension Multicall {
                 }
             }
 
-            public struct tryAggregate: ABIFunction {
+            public struct tryAggregate: ABIMultiFunction {
+                public var funcName: String = "tryAggregate"
                 public static let name = "tryAggregate"
                 public let gasPrice: BigUInt?
                 public let gasLimit: BigUInt?
@@ -83,5 +106,10 @@ extension Multicall {
                 }
             }
         }
+        
+        
     }
+    
+   
 }
+
